2c889abbb5d6c50e9016ffd02e5ed4be7d0af3a3
tiagosn01 <tiagosilva0922@gmail.com>
2020-07-31 14:57:46 -0300

    hook de form

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './App';
-import Page404 from './pages/Page404'
-import RegistrationVideo from './pages/Cadastro/RegistrationVideo'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Page404 from './pages/Page404';
+import RegistrationVideo from './pages/Cadastro/RegistrationVideo';
 import RegistrationCategory from './pages/Cadastro/RegistrationCategory';
-
-import {BrowserRouter, Switch, Route} from 'react-router-dom' 
-
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-    <Route path="/" exact component={App} />
-    <Route path="/registration/categoria" component={RegistrationCategory} />
-    <Route path="/registration/video" component={RegistrationVideo} />
-    <Route  component={Page404} />
+      <Route path="/" exact component={Home} />
+      <Route path="/registration/categoria" component={RegistrationCategory} />
+      <Route path="/registration/video" component={RegistrationVideo} />
+      <Route component={Page404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
--- src/pages/Cadastro/RegistrationCategory/index.js
+++ src/pages/Cadastro/RegistrationCategory/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 
+import useForm from '../../../hooks/useForm';
+
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
@@ -10,31 +12,16 @@ function CadastroCategoria() {
     cor: '',
   };
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
 
   useEffect(() => {
-    const url = 'https://tsflix.herokuapp.com/categorias';
+    const url = 'http://localhost:8080/categorias';
 
     fetch(url).then(async (response) => {
       const resposta = await response.json();
       setCategorias([...resposta]);
     });
   }, []);
-
-  function setValue(chave, valor) {
-    // chave: nome, descricao, bla, bli
-    setValues({
-      ...values,
-      [chave]: valor, // nome: 'valor'
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value,
-    );
-  }
 
   return (
     <PageDefault>
@@ -51,21 +38,21 @@ function CadastroCategoria() {
           values,
         ]);
 
-        setValues(valoresIniciais);
+        clearForm();
       }}
       >
 
         <FormField
           label="Nome da Categoria"
           type="text"
-          name="nome"
-          value={values.nome}
+          name="titulo"
+          value={values.titulo}
           onChange={handleChange}
         />
 
         <FormField
           label="Descrição:"
-          type="????"
+          type="text"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
@@ -114,9 +101,9 @@ function CadastroCategoria() {
       )}
 
       <ul>
-        {categorias.map((categoria, indice) => (
-          <li key={`${categoria}${indice}`}>
-            {categoria.nome}
+        {categorias.map((categoria) => (
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
